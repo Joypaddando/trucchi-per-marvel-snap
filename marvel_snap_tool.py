2e6648ff176b1fad2a8c88549a42b41b934b43f3
Joypaddando <boneccio24@tutamail.com>
2025-06-15 14:55:28 +0200

    Create marvel_snap_tool.py

---
+++ marvel_snap_tool.py
@@ -0,0 +1,6 @@
+# Simulazione tool Marvel Snap
+def simulate_resources_generation():
+    print("Marvel Snap Helper Avviato - Risorse preso in arrivo!")
+
+if __name__ == "__main__":
+    simulate_resources_generation()
